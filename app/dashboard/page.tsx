d1b1950cb710837ea2a7b18820b7e61e16c69704
ESchwenker <Eschwenker24@outlook.com>
2026-02-13 15:51:26 -0500

    books carousel

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,11 +4,9 @@ import { useAuth } from "@/context/AuthContext"
 import { useRouter } from "next/navigation"
 import { useEffect } from "react"
 import Sidebar from "../dashboard/Sidebar"
-import SummaryCard from "../dashboard/SummaryCard"
 import Header from "../dashboard/Header"
-import { books } from "@/lib/books"
 import SelectedHero from "../dashboard/SelectedHero"
-import RecommendedRow from "./RecommendedSection"
+import RecommendedSection from "./RecommendedSection"
 
 export default function Dashboard() {
   const { user, loading } = useAuth()
@@ -32,23 +30,9 @@ export default function Dashboard() {
 
         <Header />
         <SelectedHero />
-        <RecommendedRow />
+        <RecommendedSection />
 
-        <div className="dashboard__content">
-          <div className="dashboard__grid">
-            {books.map(book => (
-              <SummaryCard
-                key={book.id}
-                id={book.id}
-                title={book.title}
-                author={book.author}
-                image={book.image}
-                category={book.category}
-                time={book.time}
-              />
-            ))}
-          </div>
-        </div>
+        <div className="dashboard__content"></div>
 
       </main>
     </div>
